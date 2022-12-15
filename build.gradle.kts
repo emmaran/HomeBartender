0ee0676cd1896c0e557a17d24e48607dab135317
bgwinston <b.winston87@yahoo.com>
2022-12-14 21:58:56 -0600

    Hello World landing page

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.7-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "org.launchcode"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
