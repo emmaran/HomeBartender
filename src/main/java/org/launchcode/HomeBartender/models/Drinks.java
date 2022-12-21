225539500aa64ed386eb9916f160c9da53ad7cd8
Crishawna <crishawna.nash@gmail.com>
2022-12-21 02:46:11 -0600

    add model class Drinks

--- src/main/java/org/launchcode/HomeBartender/models/Drinks.java
+++ src/main/java/org/launchcode/HomeBartender/models/Drinks.java
@@ -1,2 +1,18 @@
-package org.launchcode.HomeBartender.models;public class Drinks {
+package org.launchcode.HomeBartender.models;
+
+import java.util.List;
+
+public class Drinks {
+
+    //this will set everything to null initially
+    private List<Cocktails> cocktails = null;
+
+    //this get for all the drinks
+    public List<Cocktails> getCocktails() {
+        return cocktails;
+    }
+    //set for all the drinks
+    public void setDrinks(List<Cocktails> cocktails) {
+        this.cocktails = cocktails;
+    }
 }
