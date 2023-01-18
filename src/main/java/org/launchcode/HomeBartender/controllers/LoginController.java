e797592a4794dce5bb03952adfe67d5a5cd814e5
Dee Chap <deniseechappell@gmail.com>
2023-01-18 17:14:49 -0600

    Fixed ambiguous mapping error in AuthenticationController by commenting out RegisterController

--- src/main/java/org/launchcode/HomeBartender/controllers/LoginController.java
+++ src/main/java/org/launchcode/HomeBartender/controllers/LoginController.java
@@ -3,49 +3,91 @@ package org.launchcode.HomeBartender.controllers;
 import org.launchcode.HomeBartender.data.LoginData;
 import org.launchcode.HomeBartender.data.UserRepository;
 import org.launchcode.HomeBartender.models.User;
+import org.launchcode.HomeBartender.models.dto.LoginFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import static org.launchcode.HomeBartender.controllers.AuthenticationController.setUserInSession;
 
 @Controller
 public class LoginController {
+
+//    I added lines 22-23 to connect it with users on 1/17
     @Autowired
     UserRepository userRepository;
-    @RequestMapping("login")
-    @ResponseBody
-    public String index() {
-        return "form";
+    @GetMapping("login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
     }
-//    need to code to display form
-@GetMapping("login")
-public String renderFormMethodName(Model model) {
 
-    return "login";
+    @PostMapping("login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
 
+        User theUser = userRepository.findByUserName(loginFormDTO.getUsername());
+
+        if (theUser == null) {
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        String password = loginFormDTO.getPassword();
+//if password from database doesn't equal password
+        if (theUser.getPwHash()!=password) {
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(), theUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("logout")
+    public String logout(HttpServletRequest request){
+        request.getSession().invalidate();
+        return "redirect:/login";
+    }
+
+//    this is what I had before chapter 19.
 }
-
-@PostMapping
-public void addLogin(@ModelAttribute LoginData loginData) {
-User user = userRepository.findByUserName(loginData.getUserName());
-};
-//    if statement to see if user isn't equal to null, you would get user passwrod. then check to see if password is equal to logindata.getpassword.
-//    if passwords match, takes to landing page, if not, error popup.
-public int getUserId(String username, String pwHash){
-    return 0;
-}
-
-//localStorage.getItem('UserID');
-
-
-
-//    @PostMapping
-
-//    for saving username and password
-//    public String processLogin(@RequestParam String parameter1) {
-//  model.addAttribute("
-//
-//        return "redirect:login";
+//    @Autowired
+//    UserRepository userRepository;
+//    @RequestMapping("login")
+//    @ResponseBody
+//    public String index() {
+//        return "form";
 //    }
-}
+////    need to code to display form
+//@GetMapping("login")
+//public String renderFormMethodName(Model model) {
+//
+//    return "login";
+//
+//}
+//
+//@PostMapping
+//public void addLogin(@ModelAttribute LoginData loginData) {
+//User user = userRepository.findByUserName(loginData.getUserName());
+//};
+
+////Aaron suggested using this.
+////localStorage.getItem('UserID');
+//
+//}
 
--- src/main/java/org/launchcode/HomeBartender/models/User.java
+++ src/main/java/org/launchcode/HomeBartender/models/User.java
@@ -1,5 +1,10 @@
 package org.launchcode.HomeBartender.models;
 
+import org.launchcode.HomeBartender.controllers.AuthenticationController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +13,9 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+
+
 
 @Entity
 
@@ -24,6 +32,12 @@ public class User {
     private String email;
 
     private String userName;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+    }
 
     public int getId() {
         return id;
@@ -56,8 +70,48 @@ public class User {
     public void setUserName(String userName) {
         this.userName = userName;
     }
-}
+
+
+
+//    not sure how to make this work.......
+//    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//    public User(String username, String password) {
+//        this.username = username;
+//        this.pwHash = encoder.encode(password);
+//    }
 //
+//    public boolean isMatchingPassword(String password) {
+//        return encoder.matches(password, pwHash);
+//    }
+
+
+//Got this from the text-Authentication
+
+//
+//    // gets the hash value stored on their object
+//    String passwordHash = user.getPwHash();
+//
+//    // hashes the submitted password
+//    String submittedpwHash = h(submittedPsw);
+//
+//if (pwHash.equals(submittedpwHash)) {
+//        // the hashes are the same, the passwords can be assumed to be the same
+//    } else {
+//        // the hashes are different, so the passwords are definitely different
+//    }
+
+//    Can't get encoder to not be red
+//    public boolean isMatchingPassword(String password) {
+//        return encoder.matches(password, pwHash);
+//    }
+//    DOn't know if I need. GOt this from Chapter 19
+//    public User(String username, String password) {
+//        this.userName = username;
+//        this.pwHash = encoder.encode(password);
+    }
+
+
+
 ////    @OneToMany(mappedBy = "user")
 ////    private final List<Event> events = new ArrayList<>();
 //
