714043eb8eb9431db64e99277b887e60dc4f5be7
SNHays22 <codergrl22@outlook.com>
2023-01-19 19:24:38 -0600

    updating

--- src/main/java/org/launchcode/HomeBartender/controllers/SearchFriendsController.java
+++ src/main/java/org/launchcode/HomeBartender/controllers/SearchFriendsController.java
@@ -1,7 +1,9 @@
 package org.launchcode.HomeBartender.controllers;
 
+import org.launchcode.HomeBartender.data.UserRepository;
 import org.launchcode.HomeBartender.models.Friends;
 import org.launchcode.HomeBartender.models.FriendsData;
+import org.launchcode.HomeBartender.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,15 +15,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("friends/search")
 public class SearchFriendsController {
 
-//    @Autowired
-//  //  private UserRepository userRepository;
-//
-//    @RequestMapping("")
-//    public String search(Model model){
-//        model.addAttribute("name");
-//        return "friends/search";
-//    }
-//
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("")
+    public String search(Model model){
+        model.addAttribute("name", userRepository.findAll());
+        return "friends/search";
+    }
+
 //    @PostMapping("results")
 //    public String displaySearchFriendsResults(Model model, @RequestParam String searchTerm){
 //        Iterable<User> user;
